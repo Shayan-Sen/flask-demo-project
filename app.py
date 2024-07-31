727ded632ca17c32f2aab3edd63e39c99b1919bc
Shayan Sen <110974961+Shayan-Sen@users.noreply.github.com>
2024-07-31 20:22:48 +0000

    Added initial html css files in template folder

--- app.py
+++ app.py
@@ -1,15 +1,11 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-  return '<h1>Hello World!</h1>'
-
-@app.route('/<name>')
-def user(name):
-  return '<h1>Hello %s!</h1>'%name
+  return render_template('home.html')
 
 
 if __name__ == '__main__':
